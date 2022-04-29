6608d0c3e390c7347986d119decad9063ac6aa1a
Kamil Piękoś <kamil.piekos@10clouds.com>
2022-04-29 17:06:41 +0200

    clear example code

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,8 +1,1 @@
 import "./assets/scss/content.scss";
-
-console.log('Content script initialized');
-
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   console.log('content', request);
-// });
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,52 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, options) => {
   return {
     entry: {
-      popup: './src/popup/popup.ts',
-      content: './src/content/content.ts',
-      background: './src/background/background.ts'
+      popup: "./src/popup/popup.ts",
+      content: "./src/content/content.ts",
+      background: "./src/background/background.ts",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist')
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
-    devtool: options.mode !== 'production' ? 'inline-source-map' : false,
+    devtool: options.mode !== "production" ? "inline-source-map" : false,
     watchOptions: {
-      ignored: 'node_modules/**'
+      ignored: "node_modules/**",
     },
     plugins: [
       new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
-        filename: '[name].css'
+        filename: "[name].css",
       }),
       new CopyPlugin({
-        patterns: [
-          {from: './src/index.html', to: ''},
-          {from: './src/manifest.json', to: ''}
-        ],
+        patterns: [{ from: "./src/static", to: "" }],
       }),
     ],
     module: {
       rules: [
         {
           test: /\.ts$/i,
-          use: 'ts-loader',
-          include: [path.resolve(__dirname, 'src')]
+          use: "ts-loader",
+          include: [path.resolve(__dirname, "src")],
         },
         {
           test: /\.(sa|sc|c)ss$/,
           use: [
             MiniCssExtractPlugin.loader,
-            'css-loader',
-            'postcss-loader',
-            'sass-loader'
-          ]
-        }
-      ]
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
+      ],
     },
     resolve: {
-      extensions: ['.ts', '.js'],
-    }
-  }
-}
+      extensions: [".ts", ".js"],
+    },
+  };
+};
